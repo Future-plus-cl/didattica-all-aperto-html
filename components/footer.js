122b3ae6327daab487c650b0d3be03ab150b1530
vincenzodivita <divitavincenzo@icloud.com>
2023-12-21 12:13:30 +0100

    Create components

---
+++ components/footer.js
@@ -0,0 +1,54 @@
+
+const footer = `<footer>
+<div class="container_footer">
+    <img src="images/logo2.jpeg" alt="" class="logo_footer">
+    <div class="sub_container_footer">
+        <div class="sub_container_footer_two">
+            <div class="title_footer">
+                sede operativa
+            </div>
+            <div class="sub_title_footer">
+                <p>via De Amicis 2560033 Chiaravalle (AN)</p> 
+            </div>
+        </div>
+        <div class="sub_container_footer_two">
+            <div class="title_footer">
+                sede legale
+            </div>
+            <div class="sub_title_footer">
+                <p>via De Amicis 2560033 Chiaravalle (AN)</p>
+            </div>
+        </div>
+        <div class="sub_container_footer_two">
+            <div class="title_footer">
+                documenti legali
+            </div>
+            <div class="sub_title_footer">
+                <p class="privacy">privacy policy</p>
+                <p>termini e condizioni</p>
+            </div>
+        </div>
+        <div class="sub_container_footer_two">
+            <div class="title_footer">
+                social network
+            </div>
+            <div class="sub_title_footer social_footer">
+                <img src="images/Youtube2.png" alt="" class="img_social">
+                <img src="images/Instagram2.png" alt="" class="img_social">
+                <img src="images/Facebook2.png" alt="" class="img_social">
+            </div>
+        </div>
+    </div>
+    
+    
+</div>
+<div class="container_footer_two">
+    <span class="span_footer">
+        © 2023 AB Company SNC P.IVA e C.F. 01555000429
+    </span>
+    <span class="span_footer">
+        sito web disegnato e realizzato da future +
+    </span>
+</div>
+
+</footer>`
---
+++ components/section-three.js
@@ -0,0 +1,233 @@
+const sectionThree = `<section class="section_three">
+<div class="container_card">
+    
+    <div class="card background_one">
+        <div class="title_card">
+        ambienti stimolanti
+        </div>
+        <div class="description_card description_type_one">
+            <p>
+                Benvenuti nel mondo innovativo di "Didattica all’ Aperto" il nuovo brand di ABcompany SNC! Siamo la punta di diamante nel mercato dell'arredamento per la didattica all'aperto, pionieri nell'offrire soluzioni straordinarie per spazi educativi all'aria aperta. 
+            </p>
+        </div>
+    </div>
+    <div class="card background_two box_shadow">
+        <div class="title_card title_type_one title_card_two">
+            con più di <span class="highlight_number">+20</span> anni di esperienza nel settore
+        </div>
+        <div class="description_card description_type_two">
+                <p>
+                    Benvenuti nel mondo innovativo di "Didattica all’ Aperto" il nuovo brand di ABcompany SNC! Siamo la punta di diamante nel mercato dell'arredamento per la didattica all'aperto, pionieri nell'offrire soluzioni straordinarie per spazi educativi all'aria aperta. 
+                </p>
+            </div>
+    </div>
+    <div class="card background_two box_shadow">
+        <div class="title_card title_type_one">
+            <div class="part_title_card_three">
+                <div class="sub_part_title_card_three">solo materiali </div>
+                <div class="sub_part_title_card_three">
+                    <img src="images/Trees2.png" alt="" class="img_trees">
+                </div>
+                
+            </div>
+            eco sostenibili 
+        </div>
+        <div class="description_card description_type_two">
+            <p>
+                I nostri prodotti sono il risultato tangibile dell'impegno costante verso l'eccellenza e l'innovazione sostenibile. Ogni pezzo del nostro arredamento per scuole è concepito con materiali ecologici e ecosostenibili, un impegno che va oltre la semplice qualità per abbracciare la responsabilità ambientale. La scelta accurata di questi materiali non solo assicura prodotti robusti e affidabili, ma anche un ambiente sano e sicuro per gli studenti.
+            </p>
+        </div>
+    </div>
+    <div class="card background_one">
+        <div class="title_card">
+            creatività e apprendimento pratico
+        </div>
+        <div class="description_card description_type_one">
+            <p>
+                Dai giochi tipo banco da lavoro ai percorsi sensoriali, ogni articolo è pensato per ispirare la creatività e promuovere un apprendimento pratico. I nostri prodotti sono più di semplici strumenti; sono il ponte tra l'educazione e la natura.
+            </p>
+        </div>
+    </div>
+</div>
+
+</section>
+<div class="container">
+<div class="container_triangle">
+    <div class="title_image">
+        showroom
+    </div>
+    <img src="images/triangle_two.png" alt="" class="triangle_two">
+</div>
+
+<section class="section_four">
+    <div class="part_section_one">
+       
+        <img src="images/eclipse.png" alt="" class="eclipse eclipse1">
+        <img src="images/eclipse.png" alt="" class="eclipse eclipse2">
+    
+        <p class="sub_title">tavoli e panche</p>
+    </div>
+    <div class="part_section_two">
+        <img src="images/eclipse.png" alt="" class="eclipse eclipse3">
+        <img src="images/eclipse.png" alt="" class="eclipse eclipse4">
+        <p class="sub_title">lavagne</p>
+    </div>
+    <div class="part_section_three">
+        <img src="images/eclipse.png" alt="" class="eclipse eclipse5">
+        <p class="sub_title five_sub_title">tribunette</p>
+        <img src="images/triangle_two.png" alt="" class="triangle_three">
+    </div>
+
+   
+
+    <div class="info">
+        <div class="title_info">
+            offriamo soluzioni su misura
+        </div>
+        <div class="description_info">
+            <p>
+                Ogni scuola merita di essere unica ed originale. Progettare intere aree didattiche è il nostro lavoro da più di vent'anni, fornendo soluzioni perfette per ogni esigenza. Hai un'idea da realizzare? Parliamone!
+            </p>
+        </div>
+    
+        <form action="post" class="form">
+  
+            <div class="container_input">
+                <div class="input">
+                    <div class="inputGroup">
+                        <input autocomplete="off" required="" type="text" id="name" name="name">
+                        <label for="name">NOME E COGNOME</label>
+                    </div>
+                </div>
+                <div class="input">
+                    <div class="inputGroup">
+                        <input autocomplete="off" required="" type="text" id="telephone" name="telephone">
+                        <label for="telephone">TELEFONO</label>
+                    </div>
+                </div>
+                <div class="input">
+                    <div class="inputGroup">
+                        <input autocomplete="off" required="" type="text" id="email" name="email">
+                        <label for="email">EMAIL</label>
+                    </div>
+                </div>
+            </div>
+            <div class="type_contact">
+                <div class="text_contact">
+                    <span>Come preferisci essere contattato?</span>
+                </div>
+                <div class="container_input_radio">
+                    <span><input type="radio" class="input_radio" name="contact" value="email">Email</span>
+                    <span><input type="radio" class="input_radio" name="contact" value="telephone">Telefono</span>
+                </div>
+            </div>
+            <div class="accept_conditions">
+                <div class="checkbox-wrapper-23">
+                    <input type="checkbox" id="check-23">
+                    <label for="check-23" style="--size: 30px">
+                        <svg viewBox="0,0,50,50">
+                        <path d="M5 30 L 20 45 L 45 5"></path>
+                        </svg>
+                    </label>
+                </div>
+                <span class="text_accept">Ho letto e accetto la politica sulla privacy ai sensi del Regolamento EU n. 679/2016</span>
+            </div>
+            <button class="send">
+                invia
+            </button>
+                  
+        </form>
+
+        <div class="title_info title_contact_principal">
+            Contattaci
+        </div>
+
+        <div class="container_contact">
+            <div class="container_contact_one">
+                <div class="title_contact title_contact_one">Scrivici un messaggio</div>
+                <div class="container_input_two">
+                    <div class="input input_contact">
+                        <div class="inputGroup">
+                            <input autocomplete="off" required="" type="text" id="name_two">
+                            <label for="name_two" class="label_contact">NOME E COGNOME</label>
+                        </div>
+                    </div>
+                    <div class="input input_contact">
+                        <div class="inputGroup">
+                            <input autocomplete="off" required="" type="text" id="telephone_two">
+                            <label for="telephone_two" class="label_contact">TELEFONO</label>
+                        </div>
+                    </div>
+                    <div class="input input_contact">
+                        <div class="inputGroup">
+                            <input autocomplete="off" required="" type="text" id="email_two">
+                            <label for="email_two" class="label_contact">EMAIL</label>
+                        </div>
+                    </div>
+                    <div class="prefer_contact">
+                        <span class="text_contact_two">Come preferisci essere contattato?</span>
+                        <div class="container_input_radio container_input_radio_two">
+                            <span><input type="radio" class="input_radio">Email </span>
+                            <span><input type="radio" class="input_radio">Telefono</span>
+                        </div>
+                    </div>
+                </div>
+               
+                <div class="input input_contact message">
+                    <div class="inputGroup">
+                        <textarea autocomplete="off" required="" type="textarea" id="message" class="input_message"></textarea>
+                        <label for="message" class="label_message">message</label>
+                    </div>
+                </div>
+            </div>
+            <div class="container_contact_two">
+                <div class="sub_container_contact_one">
+                    <div class="title_contact title_contact_one ">I nostri contatti</div>
+                    <div class="sub_container_one">
+                        <div class="sub_one">
+                            <div class="row_container">
+                                <span class="subtitle">Email</span>
+                                <span class="text_info">abcompany@gmail.com</span>
+                            </div>
+                            <div class="row_container">
+                                <span class="subtitle">whatsapp</span>
+                                <span class="text_info">+39 333 321 3215</span>
+                            </div>
+                            <div class="row_container">
+                                <span class="subtitle">Telefono</span>
+                                <span class="text_info">0712311258</span>
+                            </div>
+                        </div>
+                        <div class="sub_two">
+                            <span class="subtitle">seguici anche sui social</span>
+                            <div class="container_social">
+                                <img src="images/Youtube.png" alt="" class="social">
+                                <img src="images/Instagram.png" alt="" class="social">
+                                <img src="images/Facebook.png" alt="" class="social">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="sub_container_contact_two ">
+                    <div class="title_contact title_contact_one">dove trovarci</div>
+
+                    <div class="sub_three">
+                        <div class="row_container">
+                            <span class="subtitle">sede legale</span>
+                            <span class="text_info">Via Camerata qualcosa 12, 90003</span>
+                        </div>
+                        <div class="row_container row_two">
+                            <span class="subtitle">sede operativa</span>
+                            <span class="text_info">Via Camerata qualcosa 12, 90003</span>
+                        </div>
+                    </div>
+                    <div class="sub_map">
+                        <img src="images/map.png" alt="" class="map">
+                    </div>
+                  
+                </div>
+            </div>
+
+        </div>
+    </div>
+  </section>`
